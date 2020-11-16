cf793b9d5db826c09d59186fbc611945bca04767
Ander <anderpang@qq.com>
2020-11-16 15:59:05 +0800

    Create jt.js

---
+++ jt.js
@@ -0,0 +1,44 @@
+"use strict";
+
+(function(ctx, factory) {
+	console.log(typeof module)
+	if (typeof module === "object" && typeof
+		module.export === "object") {
+		module.export.default = factory();
+	} else {
+		ctx.jt = factory();
+	}
+})(this, function() {
+	var jt = {};
+	jt.render = function(id, data, filter) {
+		var cache = this._tmpl_cache || (this._tmpl_cache = {});
+		if (cache[id]) {
+			return cache[id](data,filter);
+		}
+		var tmpl = document.getElementById(id).innerHTML,
+			isStrTmpl = !!String.raw,
+			fun,
+			separator = isStrTmpl ? "`" : "'",
+			code = "var html=";
+		code += separator;
+		tmpl = tmpl.split(separator).join("\\" + separator);
+		if (!isStrTmpl) {
+			tmpl = tmpl.split("\n").join(" ").replace(/\$\{([^\}]+?)\}/g, function(a, b) {
+				var out = separator;
+				out += "+(";
+				out += b.split("\\\'").join("'");
+				out += ")+";
+				out += separator;
+				return out;
+			});
+		}
+		tmpl = tmpl.split("<%").join(separator + ";").split("%>").join("html+=" + separator);
+		code += tmpl;
+		code += separator;
+		code += ";return html;";
+		fun = new Function("data","filter", code);	
+		cache[id] = fun;		
+		return fun(data,filter);
+	};
+	return jt;
+});
